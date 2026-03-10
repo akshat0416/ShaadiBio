9cb1ed72642f1a417653ba8dfb30c6904a0c76e3
akshat0416 <akki200416@gmail.com>
2026-03-10 14:15:26 +0530

    Fix server port for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const path = require('path')
-const { connectDb } = require('./config/db')
-const { required, optional } = require('./config/env')
-const { createApp } = require('./app')
+const path = require("path");
+const { connectDb } = require("./config/db");
+const { required, optional } = require("./config/env");
+const { createApp } = require("./app");
 
 async function main() {
-  const port = Number(optional('PORT', '5000'))
-  const mongoUri = required('MONGO_URI')
-  const jwtSecret = required('JWT_SECRET')
-  const clientOrigin = optional('CLIENT_ORIGIN', 'http://localhost:5173')
-  const uploadDir = path.resolve(__dirname, '..', 'uploads')
+  const port = process.env.PORT || 5000;
+  const mongoUri = required("MONGO_URI");
+  const jwtSecret = required("JWT_SECRET");
+  const clientOrigin = optional("CLIENT_ORIGIN", "http://localhost:5173");
 
-  await connectDb(mongoUri)
+  const uploadDir = path.resolve(__dirname, "..", "uploads");
 
-  const app = createApp({ jwtSecret, clientOrigin, uploadDir })
+  await connectDb(mongoUri);
+
+  const app = createApp({ jwtSecret, clientOrigin, uploadDir });
+
+  app.get("/", (req, res) => {
+    res.send("ShaadiBio API is running");
+  });
+
   app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`API listening on http://localhost:${port}`)
-  })
+    console.log(`Server running on port ${port}`);
+  });
 }
 
 main().catch((err) => {
-  // eslint-disable-next-line no-console
-  console.error(err)
-  process.exit(1)
-})
-
+  console.error(err);
+  process.exit(1);
+});
